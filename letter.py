0fecb608906185bd7d7eb4eaf7ba181787357a8f
J209799 <J209799@tafe.wa.edu.au>
2023-06-04 10:26:44 +0800

    Update letter.py encryption method

--- letter.py
+++ letter.py
@@ -5,44 +5,49 @@ class _letter_return(NamedTuple):
     is_read: bool
     letter_content: str
 
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 class Letter:
     """Represents a letter."""
 
-    def __init__(self, content: str, key: int) -> None:
+    def __init__(self, content: str, addressee: str, key: str) -> None:
         self._isread = False
         self.content: str = Letter._encrypt(content, key)
+        self.addressee = addressee
 
-    def read_letter(self, key: int) -> tuple[bool, str]:
+    def read_letter(self, key: str) -> tuple[bool, str]:
         if self._isread is True:
             return _letter_return(True, Letter._decrypt(self.content, key))
         self._isread = True
         return _letter_return(False, Letter._decrypt(self.content, key))
 
     @staticmethod
-    def _encrypt(content: str, key: int) -> str:
-        encrypted_array: list[str] = []
-        offset = 1
-        for word in content.split():
-            encrypted_word = ""
-            for letter in word:
-                # Get the Unicode code with ord, then offset with the offset and key.
-                # then conver that unicode code back into a unicode character
-                encrypted_word += chr((ord(letter) + offset) + key)
-            encrypted_array.append(encrypted_word)
-            # increase the offset for each word
-            offset += 1
-        return " ".join(encrypted_array)
+    def _encrypt(content: str, key: str) -> str:
+        ciphertext = ""
+        key_index = 0
+
+        for char in content:
+            if char.upper() in alphabet:
+                shift = alphabet.index(key[key_index].upper())
+                encrypted_char = alphabet[(alphabet.index(char.upper()) + shift) % 26]
+                ciphertext += encrypted_char if char.isupper() else encrypted_char.lower()
+                key_index = (key_index + 1) % len(key)
+            else:
+                ciphertext += char
+        return ciphertext
 
     @staticmethod
-    def _decrypt(content: str, key: int):
+    def _decrypt(content: str, key: str) -> str:
         # just do the opposite of the encrypt method
-        decrypted_array: list[str] = []
-        offset = 1
-        for word in content.split():
-            decrypted_word = ""
-            for letter in word:
-                decrypted_word += chr((ord(letter) - offset) - key)
-            decrypted_array.append(decrypted_word)
-            offset += 1
-        return " ".join(decrypted_array)
+        plaintext = ""
+        key_index = 0
+
+        for char in content:
+            if char.upper() in alphabet:
+                shift = alphabet.index(key[key_index].upper())
+                decrypted_char = alphabet[(alphabet.index(char.upper()) - shift) % 26]
+                plaintext += decrypted_char if char.isupper() else decrypted_char.lower()
+                key_index = (key_index + 1) % len(key)
+            else:
+                plaintext += char
+        return plaintext
