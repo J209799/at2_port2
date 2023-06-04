f4a8cb6dca6e906d9f2960727cfb487bf0dbf8bb
J209799 <J209799@tafe.wa.edu.au>
2023-06-04 10:29:21 +0800

    implement post office

--- post_office.py
+++ post_office.py
@@ -1,5 +1,20 @@
-class PostOffice:
-    known_addresses = []
+from letter import Letter
+from letterbox import LetterBox
 
-    def __init__(self) -> None:
-        pass
+
+class PostOffice:
+
+    def __init__(self, serviced_addresses: set[LetterBox]) -> None:
+        self.serviced_addresses: set[LetterBox] = serviced_addresses
+        self.stored_letters: list[Letter] = []
+
+    def add_address(self, address: LetterBox) -> bool:
+        if address in self.serviced_addresses:
+            return False
+        else:
+            self.serviced_addresses.add(address)
+            return True
+
+    def add_letter(self, letter: Letter) -> None:
+        self.stored_letters.append(letter)
+
