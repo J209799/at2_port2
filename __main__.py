b192b7a74606ec43b1f0a0fbd365f32abfeca7c4
J209799 <J209799@tafe.wa.edu.au>
2023-06-04 10:27:30 +0800

    update __main__.py - implement what is shown in the sequence diagram

--- __main__.py
+++ __main__.py
@@ -1,10 +1,22 @@
 from correspondent import Correspondent
-from letter import Letter
+from post_office import PostOffice
+from postie import Postie
 
 if __name__ == "__main__":
-    alice = Correspondent("Alice")
-    bob = Correspondent("Bob")
-    bobs_letter = Letter("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-    bob.send_letter(alice, bobs_letter)
+    alice = Correspondent("Alice", "ABC43", "AlicesKey")
+    bob = Correspondent("Bob", "ATT3", "BobsKey")
+    charlie_postie = Postie()
+    perth_post_office = PostOffice({bob.letterbox, alice.letterbox})
+    bob_letter_content = "Lorem ipsum dolor sit amet, abcdefghi"
+    print(bob_letter_content)
+    bob.send_letter(
+        bob_letter_content,
+        "ABC43",
+        "AlicesKey",
+        perth_post_office,
+    )
+    charlie_postie.collect_postoffice_letters(perth_post_office)
+    charlie_postie.deliver_letters()
     if alice.letterbox.has_letter:
         print(alice.read_letterbox_letter())
+
