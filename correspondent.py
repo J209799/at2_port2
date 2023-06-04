11833d5752af4486ee1019c9a5bb55ebbb5d2535
J209799 <J209799@tafe.wa.edu.au>
2023-06-04 10:28:16 +0800

    update correspondent to handle encryption

--- correspondent.py
+++ correspondent.py
@@ -1,22 +1,21 @@
-from typing import Self
-
 from letter import Letter
 from letterbox import LetterBox
+from post_office import PostOffice
 
 
 class Correspondent:
-    def __init__(self, name: str, recipient_address: str, address: str) -> None:
+    def __init__(self, name: str, address: str, decryption_key: str) -> None:
         self.name = name
-        self.letterbox = LetterBox(self)
-        self.recipient_address = recipient_address
+        self.letterbox = LetterBox(self, address)
+        self._key = decryption_key
 
     def read_letterbox_letter(self) -> str | None:
         letter = self.letterbox.get_letter()
         if letter is None:
             return None
-        letter = letter.read_letter()
-        return f"Already read?: {letter.is_read}\n{letter.letter_content}"
+        letter_content = letter.read_letter(self._key)
+        return f"Already read?: {letter_content.is_read}\n{letter_content.letter_content}"
 
-    def send_letter(self, recipient: Self, letter: Letter) -> bool:
-        send_result = recipient.letterbox.add_letter(letter)
-        return send_result
+    def send_letter(self, content: str, addressee: str, encryption_key: str, post_office: PostOffice) -> None:
+        new_letter = Letter(content, addressee, encryption_key)
+        post_office.add_letter(new_letter)
